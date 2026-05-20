d235b7708b291b1376edd73cbe17066aff147ce8
yohanvillamarin <yohanvillamarin@gmail.com>
2026-05-20 18:18:43 -0500

    Configure Vite to output to docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({ 
   plugins:[react()],
-  base: '/proyect-hamburguesas/'
+  base: '/proyect-hamburguesas/',
+  build: {
+    outDir: 'docs'
+  }
 })
